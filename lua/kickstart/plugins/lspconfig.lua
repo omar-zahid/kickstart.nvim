d044679b9b46a0d660d71e721a6aa454690a8661
Omar Zahid <omarzahid@outlook.com>
2025-05-19 16:39:14 +0800

    Removed underline filter

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -168,7 +168,8 @@ return {
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
+        -- Enable if want to disable underline/highlight for errors (Example: Unused imports in Typescript)
+        -- underline = { severity = vim.diagnostic.severity.ERROR },
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',

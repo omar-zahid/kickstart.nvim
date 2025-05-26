94ce45e77d569aec0bac142080050fcc0238330a
Omar Zahid <omarzahid@outlook.com>
2025-05-26 17:49:41 +0800

    Copilot config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -53,6 +53,13 @@ vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>w', '<C-w>', { silent = true })
 
+-- Github copilot
+vim.keymap.set('n', '<leader>cp', ':Copilot panel<CR>', { desc = 'Copilot Panel' })
+vim.keymap.set('n', '<leader>cc', ':Copilot enable<CR>', { desc = 'Copilot Enable' })
+vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = 'Copilot Disable' })
+vim.keymap.set('n', '<leader>cs', ':Copilot status<CR>', { desc = 'Copilot Status' })
+
+
 -- Clear command line after pressing escape.
 -- Especially useful when just finished searching for a word or just want to clear output
 vim.keymap.set('n', '<Esc>', '<cmd>echo<CR>', { silent = true })
--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    'github/copilot.vim',
-    lazy = true,
-    cmd = { 'Copilot', 'Copilot panel', 'Copilot setup', 'Copilot status' },
-    config = function()
-      vim.keymap.set('n', '<leader>cp', ':Copilot panel<CR>', { desc = 'Copilot Panel' })
-      vim.keymap.set('n', '<leader>cc', ':Copilot enable<CR>', { desc = 'Copilot Enable' })
-      vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = 'Copilot Disable' })
-      vim.keymap.set('n', '<leader>cs', ':Copilot status<CR>', { desc = 'Copilot Status' })
-    end,
-  },
+    {
+        'github/copilot.vim',
+        lazy = true,
+        cmd = { 'Copilot', 'Copilot panel', 'Copilot setup', 'Copilot status' },
+        config = function()
+            vim.keymap.set('n', '<leader>cp', ':Copilot panel<CR>', { desc = 'Copilot Panel' })
+            vim.keymap.set('n', '<leader>cc', ':Copilot enable<CR>', { desc = 'Copilot Enable' })
+            vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = 'Copilot Disable' })
+            vim.keymap.set('n', '<leader>cs', ':Copilot status<CR>', { desc = 'Copilot Status' })
+        end,
+    },
 }

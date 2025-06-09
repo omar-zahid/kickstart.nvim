8c98066c32cbf5e0dd5f879d58d182d6678a716a
Omar Zahid <omarzahid@outlook.com>
2025-06-09 16:40:00 +0800

    Replaced typescript tools with vtsls

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -220,6 +220,7 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        vtsls = {},
         --
         eslint_d = {},
         prettierd = {},

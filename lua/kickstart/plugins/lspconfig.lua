7cc926afdcbe75d688f3a1da10a15c7ff1a0df5b
Omar Zahid <omarzahid@outlook.com>
2026-03-13 17:00:30 +0800

    Lua lsp fix

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -231,6 +231,10 @@ return {
               completion = {
                 callSnippet = 'Replace',
               },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file('', true),
+              },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               diagnostics = { globals = { 'vim' } },
             },
@@ -252,27 +256,21 @@ return {
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        "eslint_d",
-        "prettierd"
-      })
+      vim.list_extend(ensure_installed, { 'eslint_d', 'prettierd' })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      for server_name, server in pairs(servers) do
+        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+        vim.lsp.config(server_name, server)
+      end
 
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_enable = true,
+        automatic_enable = false,
         automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
       }
+
+      vim.lsp.enable(vim.tbl_keys(servers))
     end,
   },
 }

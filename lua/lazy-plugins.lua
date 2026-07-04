4010df9c5c39d7432666f2c95d7d050fe8153077
Omar Zahid <omarzahid@outlook.com>
2026-07-04 19:50:50 +0800

    Fixed ts commenting

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -79,6 +79,8 @@ require('lazy').setup({
 
   require 'kickstart.plugins.cloak',
 
+  require 'kickstart.plugins.ts-comments',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
